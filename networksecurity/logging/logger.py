65bca85a83d4584dcddb428054090eb2e95d9d49
Saurabh Pandey <saurabhpandey59254@gmail.com>
2026-03-28 03:20:59 +0530

    Completed the exception handling part

--- networksecurity/logging/logger.py
+++ networksecurity/logging/logger.py
@@ -2,14 +2,15 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-LOG_PATH = os.path.join(os.getcwd(), "logs")
-LOG_FILE_PATH = os.path.join(LOG_PATH, LOG_FILE)
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-os.makedirs(LOG_PATH, exist_ok=True)
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format='[%(asctime)s] %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
 )
